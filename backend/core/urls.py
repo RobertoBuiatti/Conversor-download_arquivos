4e955075f66d977cb1b7a0d79d0b94d2d07e6a7e
Roberto <robertobuiatti2@gmail.com>
2025-07-29 19:37:57 -0300

    fix: adiciona importação da HomeView no arquivo de URLs

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from converter.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
